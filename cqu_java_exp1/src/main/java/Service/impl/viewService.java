52b438e19ceda3ba20fbc81cbb7bf1ec607d36d6
coderxhy <1324767796@qq.com>
2024-10-11 12:09:46 +0800

    初步完成了interface
    完成了打印欢迎字符的功能

---
+++ cqu_java_exp1/src/main/java/Service/impl/viewService.java
@@ -0,0 +1,19 @@
+package Service.impl;
+
+import Service.viewInterface;
+
+public class viewService implements viewInterface {
+    public void welcomeInterface(){
+        System.out.println("欢迎使用学生成绩管理系统！！！");
+        String art =
+                "  _____ _                 _           _____ _           _ \n" +
+                        " / ____| |               | |         / ____| |         | |\n" +
+                        "| (___ | |__   __ _ _ __ | | _____  | (___ | |__   __ _| |\n" +
+                        " \\___ \\| '_ \\ / _` | '_ \\| |/ / _ \\  \\___ \\| '_ \\ / _` | |\n" +
+                        " ____) | | | | (_| | | | |   <  __/  ____) | | | | (_| | |\n" +
+                        "|_____/|_| |_|\\__,_|_| |_|_|\\_\\___| |_____/|_| |_|\\__,_|_|\n";
+        System.out.println(art);
+        //列出功能选项
+
+    }
+}
