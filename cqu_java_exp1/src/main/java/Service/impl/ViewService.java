6978a16a5ec691c9f6ef0467b5ad2f17f1bf98fc
coderxhy <1324767796@qq.com>
2024-10-12 23:45:16 +0800

    完善了interface
    增加了欢迎界面和学生界面的功能展示，新增了一个test文件

--- cqu_java_exp1/src/main/java/Service/impl/ViewService.java
+++ cqu_java_exp1/src/main/java/Service/impl/ViewService.java
@@ -5,8 +5,13 @@ import org.apache.ibatis.logging.stdout.StdOutImpl;
 import org.springframework.stereotype.Service;
 @Service
 public class ViewService implements ViewInterface {
+    public void clearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
     @Override
     public void welcomeInterface(){
+        clearScreen();
         System.out.println("欢迎使用学生成绩管理系统！！！");
         String art =
                         " W   W  EEEEE  L      CCCC  OOO  M   M  EEEEE \n" +
@@ -16,8 +21,22 @@ public class ViewService implements ViewInterface {
                         " W   W  EEEEE  LLLLL  CCCC  OOO  M   M  EEEEE \n";
 
         System.out.println(art);
-        //列出功能选项
-        System.out.println("菜单命令如下：");
-        System.out.println();
+        System.out.println("请按照提示进行操作：");
+        System.out.println("1. 学生信息管理");
+        System.out.println("2. 教师信息管理");
+        System.out.println("3. 课程信息管理");
+        System.out.println("4. 成绩信息管理");
+        System.out.println("5. 教学班信息管理");
+        System.out.println("6. 退出系统");
+    }
+    public void StudentShowInterface(){
+        clearScreen();
+        System.out.println("学生信息管理");
+        System.out.println("1. 添加学生信息");
+        System.out.println("2. 删除学生信息");
+        System.out.println("3. 修改学生信息");
+        System.out.println("4. 查看学生信息");
+        System.out.println("5. 查看所有学生信息");
+        System.out.println("6. 返回上一级");
     }
 }
--- cqu_java_exp1/src/main/java/org/example/Main.java
+++ cqu_java_exp1/src/main/java/org/example/Main.java
@@ -4,16 +4,28 @@ package org.example;
 import Service.impl.StudentService;
 import Service.impl.ViewService;
 import entity.Student;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    public static  void testStudentGenerator(){
+    @Test
+    public  void testStudentGenerator(){
         StudentService studentService = new StudentService();
         ArrayList<Student> stu=studentService.randomGenerateInfo();
         stu.forEach(studentService::viewStuBasicInfo);
     }
-    public static void main(String[] args) {
-        testStudentGenerator();
+    @Test
+    public void testViewService(){
+        ViewService viewService = new ViewService();
+        viewService.welcomeInterface();
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.nextInt()==1){
+            viewService.StudentShowInterface();
+        }
     }
+//    public static void main(String[] args) {
+//        testStudentGenerator();
+//    }
 }
